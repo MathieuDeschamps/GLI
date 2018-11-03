da7b9a57afac4f507ef82f854aef91ff6f4eb4ae
redeyes361 <redeyes_361@hotmail.com>
2018-11-03 14:49:43 +0100

    improve user service

--- src/app/services/user.service.spec.ts
+++ src/app/services/user.service.spec.ts
@@ -2,11 +2,15 @@ import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 
+// @ts-ignore
 describe('UserService', () => {
+  // @ts-ignore
   beforeEach(() => TestBed.configureTestingModule({}));
 
+  // @ts-ignore
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
+    // @ts-ignore
     expect(service).toBeTruthy();
   });
 });
